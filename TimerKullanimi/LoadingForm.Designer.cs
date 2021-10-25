4f5ee0967497afb85a36afba23989f70c9434bfc
Emin Talha Arık <arikemintalha@gmail.com>
2021-10-25 13:34:41 +0300

    Loading Panel Yapıldı

---
+++ TimerKullanimi/LoadingForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace TimerKullanimi
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.prgBar = new System.Windows.Forms.ProgressBar();
+            this.lblYukleniyor = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prgBar
+            // 
+            this.prgBar.Location = new System.Drawing.Point(12, 115);
+            this.prgBar.Name = "prgBar";
+            this.prgBar.Size = new System.Drawing.Size(374, 39);
+            this.prgBar.TabIndex = 0;
+            // 
+            // lblYukleniyor
+            // 
+            this.lblYukleniyor.AutoSize = true;
+            this.lblYukleniyor.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYukleniyor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblYukleniyor.Location = new System.Drawing.Point(88, 40);
+            this.lblYukleniyor.Name = "lblYukleniyor";
+            this.lblYukleniyor.Size = new System.Drawing.Size(230, 44);
+            this.lblYukleniyor.TabIndex = 1;
+            this.lblYukleniyor.Text = "Yükleniyor...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.BackColor = System.Drawing.Color.Transparent;
+            this.lblLoading.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLoading.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblLoading.Location = new System.Drawing.Point(326, 126);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(36, 17);
+            this.lblLoading.TabIndex = 2;
+            this.lblLoading.Text = "%25";
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 167);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.lblYukleniyor);
+            this.Controls.Add(this.prgBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoadingForm";
+            this.Text = "LoadingForm";
+            this.Load += new System.EventHandler(this.LoadingForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgBar;
+        private System.Windows.Forms.Label lblYukleniyor;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblLoading;
+    }
+}
---
+++ TimerKullanimi/LoadingForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerKullanimi
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
+
+        int loadingPanel = 0;
+
+        int colorInd = 0;
+        int colorRandom = 75;
+        Random randomColor = new Random();
+
+        private void LoadingForm_Load(object sender, EventArgs e)
+        {
+            colorInd = randomColor.Next(0, 3);
+            loadingPanel = 0;
+            if (timer1.Enabled == false) timer1.Start();
+        }
+        Random random = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            loadingPanel += random.Next(0, 3);
+            if (loadingPanel <= 100)
+            {
+                prgBar.Value = loadingPanel;
+
+                if (colorInd == 0)
+                {
+                    this.BackColor = Color.FromArgb(random.Next(75, 255), 0, 0);
+                }
+                else if (colorInd == 1)
+                {
+                    this.BackColor = Color.FromArgb(0, random.Next(75, 255), 0);
+
+                }
+                else if (colorInd == 2)
+                {
+                    this.BackColor = Color.FromArgb(0, 0, random.Next(75, 255));
+
+                }
+
+                lblLoading.Text = "%" + loadingPanel;
+                if (loadingPanel % 3 == 0) lblYukleniyor.Text = "Yükleniyor";
+                lblYukleniyor.Text += ".";
+            }
+            else if (loadingPanel > 100)
+            {
+                timer1.Stop();
+                Form1 form = new Form1();
+                form.Show();
+                this.Hide();
+            }
+        }
+    }
+}
